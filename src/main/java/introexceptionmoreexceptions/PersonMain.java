4dc59a7c86ef208ea0508d7a9bc896120586555f
Arpee78 <arpee78@gmail.com>
2024-07-05 17:50:42 +0200

    ex-introexceptionmoreexceptions

---
+++ src/main/java/introexceptionmoreexceptions/PersonMain.java
@@ -0,0 +1,12 @@
+package introexceptionmoreexceptions;
+
+public class PersonMain {
+
+    public static void main(String[] args) {
+        Person person = new Person(null, 32);
+        System.out.println(person.getName());
+//        System.out.println(person.getName().toUpperCase());
+    }
+
+
+}
